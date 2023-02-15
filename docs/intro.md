079174e90ae04e7a17a04125c822a8ebd112d243
Siddhant Rath <sid@tamu.edu>
2023-02-15 10:06:28 -0600

    add docs

--- docs/intro.md
+++ docs/intro.md
@@ -6,9 +6,15 @@ sidebar_position: 1
 
 DAIKON is a framework developed for  pharma–academic consortium-based drug discovery efforts to share ideas, technologies, compounds, assays, projects and portfolios. 
 
+In the field of drug discovery, developing new therapeutic agents is a complex and time-consuming process that requires significant resources, expertise, and collaboration. To support the drug discovery process, a comprehensive knowledge capture and project management tool can be an invaluable asset. This tool can help researchers capture and organize important scientific data, streamline communication and collaboration, and manage the workflow from early-stage drug discovery through clinical development.
+
 :::warning Active Development
 The framework is under heavy development. Documents are updated frequently.
 :::
+
+The drug discovery knowledge capture and project management tool provides a centralized platform that enables researchers to track and analyze data, plan and execute experiments, and collaborate with team members. This tool allows researchers to capture and store data in a structured and organized way, making it easier to search and retrieve information when needed. Additionally, the project management features help keep everyone on the same page and on track towards achieving project milestones and goals.
+
+The tool can help speed up the drug discovery process and reduce the risk of errors or oversights. It also helps to ensure that research teams are working together efficiently, resulting in more successful drug discovery outcomes. With the right drug discovery knowledge capture and project management tool, researchers can accelerate the development of new therapeutic agents, and ultimately, improve patient outcomes.
 
 ### What you'll need
 
@@ -23,8 +29,10 @@ The framework is under heavy development. Documents are updated frequently.
 
 Start by cloning daikon-core-server and daikon-core-webapp**.
 
+Daikon uses docker that simplifies software deployment by packaging applications and their dependencies into portable containers that can be easily deployed across different environments. This helps eliminate compatibility issues and allows for faster and more efficient deployment of software
+
 ## Follow our qucik start guides
-Guides to start the project are under "Deployment Guide" section
+Guides to start the project are under **"Deployment Guide"** section
 
 :::caution Development & Testing
 The framework is in beta. 
--- src/components/HomepageFeatures/HomePageScope.js
+++ src/components/HomepageFeatures/HomePageScope.js
@@ -1,18 +1,69 @@
-import React from 'react'
+import React from "react";
 import styles from "./styles.module.css";
 import clsx from "clsx";
 
 const HomePageScope = () => {
   return (
-
-    <div className="container" style={{ paddingTop: "5em", background: "rgba(255,255,255, 0.4)", borderRadius: "10px" }}>
-      <h1 className="hero__title">Positioned right at the early drug discovery research phase.</h1>
+    <div
+      className="container"
+      style={{
+        paddingTop: "5em",
+        paddingBottom: "5em",
+        background: "rgba(255,255,255, 0.4)",
+        borderRadius: "10px",
+      }}
+    >
+      <h1 className="hero__title">
+        Positioned right at the early drug discovery research phase.
+      </h1>
       <div style={{ margin: "40px" }}>
         <img src="/daikon/img/Landing/Scope.png" />
       </div>
+      <div className="diagonalBox">
+        <div className="row">
+          <div className="text--left">
+            DAIKON is a comprehensive drug discovery management tool designed to
+            support the drug discovery process from target identification to
+            pre-clinical development. The tool is intended to be used by
+            pharmaceutical and biotech companies, academic researchers, and
+            other organizations involved in drug discovery.
+            <ul>
+              <li>
+                Data capture and organization: DAIKON provides a centralized
+                platform to capture and organize data related to drug discovery,
+                including experimental data. The tool allows users to store,
+                search, and retrieve data in a structured and organized way.
+              </li>
+              <li>
+                Project management: DAIKON includes project management features
+                to help teams track project progress, manage tasks, and
+                collaborate effectively. The tool enables users to set
+                milestones, assign tasks, and monitor progress, ensuring that
+                the project stays on track.
+              </li>
+              <li>
+                Collaboration: DAIKON supports collaboration among team members
+                by providing a platform for communication and knowledge sharing.
+                The tool enables users to share data, exchange feedback, and
+                communicate with team members in real-time.
+              </li>
+              <li>
+                Data analysis: DAIKON includes data analysis features to help
+                users make informed decisions about drug development. The tool
+                provides visualization and analysis tools to help users identify
+                trends and patterns in the data.
+              </li>
+            </ul>
+            DAIKON is a comprehensive drug discovery management tool designed to
+            support the drug discovery process. The tool includes data capture
+            and organization, project management, collaboration, and data
+            analysis to help teams streamline their workflows, improve
+            communication, and accelerate drug development.
+          </div>
+        </div>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default HomePageScope
+export default HomePageScope;
