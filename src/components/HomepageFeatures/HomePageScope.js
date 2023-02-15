f68acea362ab870c4c45c67019feaf884e48f548
Siddhant Rath <sid@tamu.edu>
2023-02-15 10:37:40 -0600

    guides

--- src/components/HomepageFeatures/HomePageScope.js
+++ src/components/HomepageFeatures/HomePageScope.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./styles.module.css";
 import clsx from "clsx";
+import { Box } from "grommet";
 
 const HomePageScope = () => {
   return (
@@ -13,55 +14,63 @@ const HomePageScope = () => {
         borderRadius: "10px",
       }}
     >
-      <h1 className="hero__title">
-        Positioned right at the early drug discovery research phase.
-      </h1>
-      <div style={{ margin: "40px" }}>
-        <img src="/daikon/img/Landing/Scope.png" />
-      </div>
-      <div className="diagonalBox">
-        <div className="row">
-          <div className="text--left">
-            DAIKON is a comprehensive drug discovery management tool designed to
-            support the drug discovery process from target identification to
-            pre-clinical development. The tool is intended to be used by
-            pharmaceutical and biotech companies, academic researchers, and
-            other organizations involved in drug discovery.
-            <ul>
-              <li>
-                Data capture and organization: DAIKON provides a centralized
-                platform to capture and organize data related to drug discovery,
-                including experimental data. The tool allows users to store,
-                search, and retrieve data in a structured and organized way.
-              </li>
-              <li>
-                Project management: DAIKON includes project management features
-                to help teams track project progress, manage tasks, and
-                collaborate effectively. The tool enables users to set
-                milestones, assign tasks, and monitor progress, ensuring that
-                the project stays on track.
-              </li>
-              <li>
-                Collaboration: DAIKON supports collaboration among team members
-                by providing a platform for communication and knowledge sharing.
-                The tool enables users to share data, exchange feedback, and
-                communicate with team members in real-time.
-              </li>
-              <li>
-                Data analysis: DAIKON includes data analysis features to help
-                users make informed decisions about drug development. The tool
-                provides visualization and analysis tools to help users identify
-                trends and patterns in the data.
-              </li>
-            </ul>
-            DAIKON is a comprehensive drug discovery management tool designed to
-            support the drug discovery process. The tool includes data capture
-            and organization, project management, collaboration, and data
-            analysis to help teams streamline their workflows, improve
-            communication, and accelerate drug development.
+      <Box
+        direction="column"
+        border={{ color: "#332288", size: "large" }}
+        pad="medium"
+      >
+        <h1 className="hero__title">
+          Positioned right at the early drug discovery research phase.
+        </h1>
+        <div style={{ margin: "40px" }}>
+          <img src="/daikon/img/Landing/Scope.png" />
+        </div>
+        <div style={{padding:"2em"}}>
+          <div className="row">
+            <div className="text--left">
+              DAIKON is a comprehensive drug discovery management tool designed
+              to support the drug discovery process from target identification
+              to pre-clinical development. The tool is intended to be used by
+              pharmaceutical and biotech companies, academic researchers, and
+              other organizations involved in drug discovery.
+              <ul>
+                <li>
+                  Data capture and organization: DAIKON provides a centralized
+                  platform to capture and organize data related to drug
+                  discovery, including experimental data. The tool allows users
+                  to store, search, and retrieve data in a structured and
+                  organized way.
+                </li>
+                <li>
+                  Project management: DAIKON includes project management
+                  features to help teams track project progress, manage tasks,
+                  and collaborate effectively. The tool enables users to set
+                  milestones, assign tasks, and monitor progress, ensuring that
+                  the project stays on track.
+                </li>
+                <li>
+                  Collaboration: DAIKON supports collaboration among team
+                  members by providing a platform for communication and
+                  knowledge sharing. The tool enables users to share data,
+                  exchange feedback, and communicate with team members in
+                  real-time.
+                </li>
+                <li>
+                  Data analysis: DAIKON includes data analysis features to help
+                  users make informed decisions about drug development. The tool
+                  provides visualization and analysis tools to help users
+                  identify trends and patterns in the data.
+                </li>
+              </ul>
+              DAIKON is a comprehensive drug discovery management tool designed
+              to support the drug discovery process. The tool includes data
+              capture and organization, project management, collaboration, and
+              data analysis to help teams streamline their workflows, improve
+              communication, and accelerate drug development.
+            </div>
           </div>
         </div>
-      </div>
+      </Box>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import HomePageScope from "@site/src/components/HomepageFeatures/HomePageScope";
 import styles from "./index.module.css";
-
+import { Box } from "grommet";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -14,36 +14,78 @@ function HomepageHeader() {
     <div>
       <header className={clsx("hero", styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">
-            <b>Open source</b> foundational framework <i>integrating</i> <b>target-based</b> drug discovery workflows.
+          <h1
+            className="hero__title"
+            style={{ letterSpacing: "0.2em", textTransform: "capitalize" }}
+          >
+            {/* {siteConfig.title} */}DAIKON
+          </h1>
+          <p className="hero__subtitle" style={{ color: "#332288" }}>
+            <b>Open source</b> foundational framework <i>integrating</i>{" "}
+            <b>target-based</b> drug discovery workflows.
           </p>
-
           <p>
-            Deploy on-premise or on cloud and bring in your Organization's users
-            quickly with Active Directory Connect.
+            <b>
+              Deploy on-premise or on cloud and bring in your Organization's
+              users quickly with Active Directory Connect. DAIKON is a framework
+              developed for pharma–academic consortium-based drug discovery
+              efforts to share ideas, technologies, compounds, assays, projects
+              and portfolios.
+            </b>
           </p>
-          <div className={styles.buttons}>
-            <Link
-              className="button button--secondary button--md"
-              to="/docs/intro"
-            >
-              Developers Guide
-            </Link>
-          </div>
+          <Box
+            direction="row"
+            pad="medium"
+            align="center"
+            alignContent="center"
+            alignSelf="center"
+            gap="medium"
+            justify="center"
+          >
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--md"
+                to="/docs/category/deployment-manual"
+              >
+                Deployment Manual
+              </Link>
+            </div>
+
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--md"
+                to="/docs/category/user-guide"
+              >
+                User Guide
+              </Link>
+            </div>
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--md"
+                to="/docs/category/admin-guide"
+              >
+                Admin Guide
+              </Link>
+            </div>
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--md"
+                to="/docs/intro"
+              >
+                Developers Manual
+              </Link>
+            </div>
+          </Box>
         </div>
       </header>
     </div>
-
   );
 }
-
-
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <div >
+    <div>
       <Layout
         title={`Home`}
         description="Description will go into a meta tag in <head />"
